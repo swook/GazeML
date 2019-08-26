36189a13764d79e9b75a6a5f883f2b0c975b6003
sihoon08 <50740152+sihoon08@users.noreply.github.com>
2019-08-26 14:55:30 +0900

    Update elg_demo.py

--- src/elg_demo.py
+++ src/elg_demo.py
@@ -8,191 +8,14 @@ import time
 
 import coloredlogs
 import cv2 as cv
-import cv2 as cv2
 import numpy as np
 import tensorflow as tf
 
-# from util.calibration import *
 from datasources import Video, Webcam
 from models import ELG
 import util.gaze
 
-start_condition = False
-is_detect = False
-
-###################################### Start Cali #############################################
-Const_Cali_Window_name = 'canvas'
-# Const_Display_X = 1680
-# Const_Display_Y = 1050
-
-Const_Display_X = 400
-Const_Display_Y = 300
-
-Const_Cali_Num_X = 3
-Const_Cali_Num_Y = 3
-Const_Cali_Radius = 30
-Const_Cali_Resize_Radius = 7
-
-
-Const_Cali_Move_Duration = 0.5          # 캘리브레이션 원 이동 속도
-Const_Cali_Caputure_Duration = 0.8      # 캘리브레이션 원 줄어드는 속도
-
-Const_Cali_Margin_X = 50
-Const_Cali_Margin_Y = 50
-
-Const_Cali_Cross_Size = 16
-
-
-Cali_Center_Points = []
-
-
-sequence = queue.Queue()
-
-
-
-
-def start_cali():
-
-    # 큐에 캘리브레이션 순서 인덱스를 찾례로 넣는다
-    for i in range(0, Const_Cali_Num_X * 1):
-        sequence.put_nowait(i)
-
-    img = init_canvas()
-    background = img.copy()
-    init_cali()
-
-    draw_circle(img, Cali_Center_Points[0], Const_Cali_Radius)
-    draw_cross(img, Cali_Center_Points[0])
-
-    for point in Cali_Center_Points:
-        draw_circle(img, point, Const_Cali_Radius)
-        draw_cross(img, point)
-    # 큐의 순서대로 캘리브레이션 시작
-    index = sequence.get_nowait()
-    resize_figure(img, Cali_Center_Points[index], Const_Cali_Radius, Const_Cali_Caputure_Duration, background)
-
-    cv2.waitKey(0)
-    close_window(Const_Cali_Window_name)
-    return
-
-
-def move_figure(img, start_point, end_point, current_point, duration, background, count = 0):
-
-    global is_detect
-    while( is_detect == False):
-        continue
-
-    img = background.copy()
-
-    Const_Unit_Time = 60        # 0.01초마다 실행
-    to_move_x = (end_point[0] - start_point[0])
-    to_move_y = (end_point[1] - start_point[1])
-
-    move_once_x = to_move_x / (duration * Const_Unit_Time)
-    move_once_y = to_move_y / (duration * Const_Unit_Time)
-
-    updated_current_point = (current_point[0] + move_once_x , current_point[1] + move_once_y)
-
-    draw_circle(img, current_point, Const_Cali_Radius)
-    draw_cross(img, current_point)
-
-    display_canvas(Const_Cali_Window_name,img)
-    count = count +1 
-
-    if (count == (duration * Const_Unit_Time)):
-        resize_figure(img, end_point, Const_Cali_Radius, Const_Cali_Caputure_Duration, background )
-        return
-
-    # threading.Timer(1 / Const_Unit_Time, move_figure, [img, start_point, end_point, updated_current_point, duration, background, count]).start()
-    th = threading.Timer(1 / Const_Unit_Time, move_figure, [img, start_point, end_point, updated_current_point, Const_Cali_Move_Duration, background, count])
-    th.daemon = True
-    th.start()
-
-
-def resize_figure(img, point, current_radius, duration, background, count = 0):
-
-
-
-
-    img = background.copy()
-
-    Const_Unit_Time = 60        # 0.01초마다 실행
-
-    to_resize_radius = Const_Cali_Radius - Const_Cali_Resize_Radius
-    resize_once_radius = to_resize_radius    / (duration * Const_Unit_Time)
-
-    updated_current_radius = current_radius - resize_once_radius
-    draw_circle(img, point, updated_current_radius)
-    draw_cross(img, point)
-
-    display_canvas(Const_Cali_Window_name,img)
-    count = count + 1
-
-    global is_detect
-    while( is_detect == False):
-        continue
-
-    # 캘리브레이션 순간!
-    if(count == (duration * Const_Unit_Time)):
-        # 다음 캘리브레이션 경로가 없다면 창 종료
-        if (sequence.empty() == True):
-            return
-        ##########################################
-        # to-do : 눈의 좌표 저장 
-
-        ##########################################
-
-        # 큐에 다음 캘리브레이션 포인트가 있다면 원을 이동하여 캘리브레이션 작업
-        index = sequence.get_nowait()
-        move_figure(img, point, Cali_Center_Points[index], point, Const_Cali_Move_Duration, background )
-        return
-
-    th = threading.Timer(1 / Const_Unit_Time, resize_figure, [img, point, updated_current_radius, duration, background, count])
-    th.daemon = True
-    th.start()
-
-def init_cali():
-    cali_unit_distance_x = (Const_Display_X - Const_Cali_Margin_X * 2) / (Const_Cali_Num_X - 1)
-    cali_unit_distance_y = (Const_Display_Y - Const_Cali_Margin_Y * 2) / (Const_Cali_Num_Y - 1)
-
-
-    for y in range(0, Const_Cali_Num_Y):
-        for x in range(0, Const_Cali_Num_X):
-            Cali_Center_Points.append(((int)(Const_Cali_Margin_X + cali_unit_distance_x * x) 
-                                     , (int)(Const_Cali_Margin_Y + cali_unit_distance_y * y) ))
-
-
-
-def init_canvas():
-    img = np.zeros((Const_Display_Y, Const_Display_X, 3), np.uint8)
-    return img
-
-def draw_circle(img , point, radius):
-    img = cv2.circle(img, ((int)(point[0]),(int)(point[1])), (int)(radius), (0,0,255), -1)
-
-def draw_cross(img , point):
-    half_size = (int)(Const_Cali_Cross_Size / 2)
-    img = cv2.line(img, ((int)(point[0] - half_size), (int)(point[1])) , ((int)(point[0] + half_size), (int)(point[1])) , (255,255,255),1)
-    img = cv2.line(img, ((int)(point[0]) , (int)(point[1] - half_size)) , ((int)(point[0]) , (int)(point[1] + half_size)) , (255,255,255),1)
-
-
-def display_canvas(canvas_name , img):
-    # Display On Canvas
-    # cv2.namedWindow(canvas_name, cv2.WINDOW_NORMAL);  # auto resized
-    # cv2.setWindowProperty(canvas_name, cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-    cv2.imshow(canvas_name, img)
-    None
-
-def close_window(canvas_name):
-    # cv2.destroyWindow(canvas_name)
-    cv2.destroyAllWindows()
-
-
-###################################### End Cali #############################################3
-
-
 if __name__ == '__main__':
-
 
     # Set global log level
     parser = argparse.ArgumentParser(description='Demonstration of landmarks localization.')
@@ -217,13 +40,10 @@ if __name__ == '__main__':
     from tensorflow.python.client import device_lib
     session_config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
     gpu_available = False
-
-
     try:
         gpus = [d for d in device_lib.list_local_devices(session_config)
                 if d.device_type == 'GPU']
         gpu_available = len(gpus) > 0
-
     except:
         pass
 
@@ -248,10 +68,7 @@ if __name__ == '__main__':
                                  data_format='NCHW' if gpu_available else 'NHWC',
                                  eye_image_shape=(36, 60))
 
-
-
         # Define model
-
         if args.from_video:
             model = ELG(
                 session, train_data={'videostream': data_source},
@@ -318,10 +135,7 @@ if __name__ == '__main__':
         # Begin visualization thread
         inferred_stuff_queue = queue.Queue()
 
-
-
         def _visualize_output():
-
             last_frame_index = 0
             last_frame_time = time.time()
             fps_history = []
@@ -329,61 +143,38 @@ if __name__ == '__main__':
 
             # 패턴
 
-            pattern = [0, 1, 3, 9, 7]
+            pattern = [1, 3, 9, 7]
             before_history = 0
             after_history = 0
-            pattern_compare = [0]
+            pattern_compare = []
             match = 0
-            gaze_mean = 0
-            point = 0
-            
+
             if args.fullscreen:
                 cv.namedWindow('vis', cv.WND_PROP_FULLSCREEN)
                 cv.setWindowProperty('vis', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
 
             while True:
-                global is_detect
                 # If no output to visualize, show unannotated frame
                 if inferred_stuff_queue.empty():
                     next_frame_index = last_frame_index + 1
                     if next_frame_index in data_source._frames:
-
                         next_frame = data_source._frames[next_frame_index]
-                        # print( 'len : %d  //// faces in : %d ',len(next_frame['faces']), 'faces' in next_frame )
                         if 'faces' in next_frame and len(next_frame['faces']) == 0:
-
-
-                            is_detect = False
-
                             if not args.headless:
-                                # cv.imshow('vis', next_frame['bgr'])
-                                None
-
+                                cv.imshow('vis', next_frame['bgr'])
                             if args.record_video:
                                 video_out_queue.put_nowait(next_frame_index)
                             last_frame_index = next_frame_index
-
-                        elif not 'faces' in next_frame :
-                            is_detect = True
-                            global start_condition
-                            if (start_condition == False):
-                                None
-                                start_condition = True
-                                calibration_thread = threading.Thread(target=start_cali, name='calibration_th2')
-                                calibration_thread.daemon = True
-                                calibration_thread.start()
-
-                    # print(is_detect) 
-
-                    #/////////////////////////////////////////////////////
-                    # if cv.waitKey(1) & 0xFF == ord('q'):
-                    #     return
-                    #/////////////////////////////////////////////////////
+                    if cv.waitKey(1) & 0xFF == ord('q'):
+                        return
                     continue
 
                 # Get output from neural network and visualize
                 output = inferred_stuff_queue.get()
                 bgr = None
+
+
+
                 for j in range(batch_size):
                     frame_index = output['frame_index'][j]
                     if frame_index not in data_source._frames:
@@ -420,19 +211,19 @@ if __name__ == '__main__':
                                                                      .reshape(-1, 1, 2)],
                             isClosed=True, color=(255, 255, 0), thickness=1, lineType=cv.LINE_AA,
                         )
-                    # if can_use_iris:
-                    #     cv.polylines(
-                    #         eye_image_annotated,
-                    #         [np.round(eye_upscale*eye_landmarks[8:16]).astype(np.int32)
-                    #                                                   .reshape(-1, 1, 2)],
-                    #         isClosed=True, color=(0, 255, 255), thickness=1, lineType=cv.LINE_AA,
-                    #     )
-                        # cv.drawMarker(
-                        #     eye_image_annotated,
-                        #     tuple(np.round(eye_upscale*eye_landmarks[16, :]).astype(np.int32)),
-                        #     color=(0, 255, 255), markerType=cv.MARKER_CROSS, markerSize=4,
-                        #     thickness=1, line_type=cv.LINE_AA,
-                        # )
+                    if can_use_iris:
+                        cv.polylines(
+                            eye_image_annotated,
+                            [np.round(eye_upscale*eye_landmarks[8:16]).astype(np.int32)
+                                                                      .reshape(-1, 1, 2)],
+                            isClosed=True, color=(0, 255, 255), thickness=1, lineType=cv.LINE_AA,
+                        )
+                        cv.drawMarker(
+                            eye_image_annotated,
+                            tuple(np.round(eye_upscale*eye_landmarks[16, :]).astype(np.int32)),
+                            color=(0, 255, 255), markerType=cv.MARKER_CROSS, markerSize=4,
+                            thickness=1, line_type=cv.LINE_AA,
+                        )
                     face_index = int(eye_index / 2)
                     eh, ew, _ = eye_image_raw.shape
                     v0 = face_index * 2 * eh
@@ -447,8 +238,6 @@ if __name__ == '__main__':
                     frame_landmarks = (frame['smoothed_landmarks']
                                        if 'smoothed_landmarks' in frame
                                        else frame['landmarks'])
-
-
                     for f, face in enumerate(frame['faces']):
                         for landmark in frame_landmarks[f][:-1]:
                             cv.drawMarker(bgr, tuple(np.round(landmark).astype(np.int32)),
@@ -460,10 +249,9 @@ if __name__ == '__main__':
                             color=(0, 255, 255), thickness=1, lineType=cv.LINE_AA,
                         )
 
-
                     # Transform predictions
                     # 눈 중심 변경
-                    
+
                     eye_landmarks = np.concatenate([eye_landmarks,
                                                     [[eye_landmarks[-1, 0] + eye_radius,
                                                       eye_landmarks[-1, 1]]]])
@@ -553,15 +341,14 @@ if __name__ == '__main__':
 
                         gaze_history.append(current_gaze)
                         gaze_history_max_len = 10
-                        
                         if len(gaze_history) > gaze_history_max_len:
                             gaze_history = gaze_history[-gaze_history_max_len:]
-                            
+
                         # 시선 좌표 변경
 
                         gaze_mean = np.mean(gaze_history, axis=0)
                         util.gaze.draw_gaze(bgr, iris_centre, gaze_mean,
-                                            thickness=1)                            
+                                            thickness=1)
 
                     else:
                         gaze_history.clear()
@@ -612,10 +399,7 @@ if __name__ == '__main__':
                                    fontFace=cv.FONT_HERSHEY_DUPLEX, fontScale=0.79,
                                    color=(255, 255, 255), thickness=1, lineType=cv.LINE_AA)
                         if not args.headless:
-
-
-                            # cv.imshow('vis', bgr)
-                            None
+                            cv.imshow('vis', bgr)
                         last_frame_index = frame_index
 
                         # Record frame?
@@ -626,14 +410,13 @@ if __name__ == '__main__':
                         # 패턴 매치
 
                         if (cv.waitKey(1) & 0xFF == ord('q')) | match == len(pattern):
-                           return
+                            return
 
                         # Print timings
-                        if frame_index % 60 == 0:
+                        if frame_index % 10 == 0:
                             latency = _dtime('before_frame_read', 'after_visualization')
                             processing = _dtime('after_frame_read', 'after_visualization')
                             timing_string = ', '.join([
-
                                 _dstr('read', 'before_frame_read', 'after_frame_read'),
                                 _dstr('preproc', 'after_frame_read', 'after_preprocessing'),
                                 'infer: %dms' % int(frame['time']['inference']),
@@ -665,50 +448,33 @@ if __name__ == '__main__':
                                     match = match + 1
                                 else :
                                     match = 0
-                                    pattern_compare = [0]
+                                    pattern_compare = []
                                     break
                                 i = i + 1
-
-        ## End visualize_output ##
-
-        # calibration_thread = threading.Thread(target=start_cali, name='calibration_th2')
-        # calibration_thread.daemon = True
-        # calibration_thread.start()
-
 
         visualize_thread = threading.Thread(target=_visualize_output, name='visualization')
         visualize_thread.daemon = True
         visualize_thread.start()
 
-
-
         # Do inference forever
         infer = model.inference_generator()
-
-
         while True:
-
             output = next(infer)
-
             for frame_index in np.unique(output['frame_index']):
                 if frame_index not in data_source._frames:
                     continue
                 frame = data_source._frames[frame_index]
-
                 if 'inference' in frame['time']:
                     frame['time']['inference'] += output['inference_time']
                 else:
                     frame['time']['inference'] = output['inference_time']
             inferred_stuff_queue.put_nowait(output)
 
-
             if not visualize_thread.isAlive():
                 break
 
             if not data_source._open:
                 break
-
-
 
         # Close video recording
         if args.record_video and video_out is not None:
